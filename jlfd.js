5a57ea32a037c0f890dc82b7fc62529f78e95537
cvgfe <56747794+cvgfe@users.noreply.github.com>
2019-12-08 22:39:43 +0800

    Add files via upload

---
+++ jlfd.js
@@ -0,0 +1,15 @@
+
+ 
+ 
+ window.addEventListener('resize',()=>{
+    add = function(){
+        console.log("好难")
+    }
+    setInterval(add,3000);
+})
+function btn(){
+    change = function(){
+        alert("hello");
+    }
+    setTimeout(change,300);
+}
